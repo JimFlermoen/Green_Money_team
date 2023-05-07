b57bdea86f3744b0a9a050c672508662e03d0064
Jim Flermoen <james.flermoen@gmail.com>
2023-05-07 01:22:20 +0000

    updateing files

--- gen.py
+++ gen.py
@@ -1,13 +1,36 @@
+# (REPL) enter: python3.7 -i gen.py
+
+# >>> generator = gen_range(10)
+# >>> for num in gen_range(10, step=2):
+#....    print(num)
+# runs until stop interator
 def gen_range(stop, start=1, step=1):
     num = start
     while num <= stop:
         yield num
         num += step
-        
+ 
+ #
+ 
+ # Converting generator to list
+    # >>> generator = gen_range(10)
+    # >>> list(Gemerator)
+ 
+ #-------------------------------------------------------------------------
+ # (REPL) enter: python3.7 -i gen.py
+ 
+ # >>> fib = gen_fib()
+ # >>> next(fib)
+ 
 def gen_fib():
     a, b = 0, 1
     while True:
         a, b = b, a + b
         yield a
-        
-        
+    
+# >>> list(fib) would run for ever and never return a number.
+
+# how to get giant list and get the 50th number in the sequence.
+# >>> fib = gen_fib()
+# >>> [next(fib) for _ in range (50)][-1]
+# output: 12586269025
--- testing_fuction_lab.py
+++ testing_fuction_lab.py
@@ -1,4 +1,5 @@
 # 1) Write a `split_name` function that takes a string and returns a dictionary with first_name and last_name
+
 def split_name(name):
     first_name, last_name = name.split(maxsplit=1)
     return {
@@ -19,6 +20,7 @@ assert split_name("Victor Von Doom") == {
 }, f"Expected {{'first_name': 'Victor', 'last_name': 'Von Doom'}} but received {split_name('Victor Von Doom')}"
 
 # 3) Write an `is_palindrome` function to check if a string is a palindrome (reads the same from left-to-right and right-to-left)
+
 def is_palindrome(item):
     item = str(item)
     return item == item[::-1]
@@ -32,6 +34,7 @@ assert is_palindrome(101) == True, f"Expected True but got {is_palindrome(101)}"
 assert is_palindrome(10) == False, f"Expected False but got {is_palindrome(10)}"
 
 # 5) Write a `build_list` function that takes an item and a number to include in a list
+
 def build_list(item, count=1):
     items = []
     for _ in range(count):
